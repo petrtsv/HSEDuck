7fb87b8467c78cc1f16b73a1c8e07ed7979262c4
Petr Tsvetkov <petr.tsv@gmail.com>
2021-12-12 03:57:58 +0300

    Steal greeting from @khbminus

--- hseduck_bot/telegram/commands/start.py
+++ hseduck_bot/telegram/commands/start.py
@@ -5,4 +5,6 @@ from hseduck_bot.telegram.template_utils import get_text
 
 
 def run(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=get_text('start'), parse_mode='HTML')
+    name = update.effective_user.first_name
+    context.bot.send_message(chat_id=update.effective_chat.id, text=get_text('start', {'name': name}),
+                             parse_mode='HTML')
